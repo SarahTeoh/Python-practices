c40b27a6ad46685b95545bd4be1171e67e0810a6
Sarah Teoh <you@example.com>
2020-04-24 22:17:02 +0800

    Implement calculation of distance between two locations on earth

--- 第2回/calc_distance.py
+++ 第2回/calc_distance.py
@@ -1,18 +1,43 @@
 import math
+import csv
+import pandas as pd
 
-def hav(sigma1, lambda1, sigma2, lambda2):
-    sin**2
-    return hav_answer
+def hav(theta):
+    return math.sin(theta/2)**2
 
-def calc_distance():
+#Calculate between 2 points on earth using longtitude and latitude
+def calc_distance(location1, location2):
+    print("location1:")
+    print(location1[0])
+    print(location1[1])
+    print(location2[0])
+    print(location2[1])
     r = 6378.1
-    inside_square_root = math.sqrt()
-    distance = 2 * r * math.asin()
+    distance = 2 * r * math.asin(math.sqrt(hav(location2[0]-location1[0]) + math.cos(location1[0]) * math.cos(location2[0]) * hav(location2[1]-location1[1])))
 
     return distance
 
 def main():
-    calc_distance()
+    #Read data from csv file
+    loc_df = pd.read_csv('locations.csv')
+    #Convert to dictionaty
+    loc_dict = loc_df.set_index('name').T.apply(tuple).to_dict()
+
+    #Convert degree to radians
+    for k, v in loc_dict.items():
+        loc_dict[k] = tuple(map(math.radians, v))
+
+    #Get user's origin and destination input
+    origin = input("Please input origin: ")
+    destination = input("Please input destination: ")
+
+    #latitude, longtitude of origin and destination
+    origin_loc = loc_dict.get(origin, "Does not exist")
+    dest_loc = loc_dict.get(destination, "Does not exist")
+
+    print("from: ", origin )
+    print("to: ", destination)
+    print("distance: ", calc_distance(origin_loc, dest_loc), "km")
 
 if __name__ == '__main__':
     main()
