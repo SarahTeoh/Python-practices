58a19c2b05fa68b26c907302034764d9cfb2e94a
Sarah Teoh <you@example.com>
2020-05-18 13:13:55 +0800

    Change to function

--- 4th/kaiSquare.py
+++ 4th/kaiSquare.py
@@ -6,15 +6,10 @@ from scipy.stats import norm
 import numpy as np
 import math
 
-M = 5000 #Repetition times
-N = 5000 #Number of data
-k = int(1 + math.log2(N)) #Sturges' formula
-kai_list = []
-
-for i in range(M):
-	num_list = np.random.randn(N) #Generate N random numbers
-	bins = k
-	freq, max_of_bins = np.histogram(num_list, bins=k) #Make histogram
+def calc_chi(data_array):
+	N = data_array.size
+	k = int(1 + math.log2(N)) #Sturges' formula
+	freq, max_of_bins = np.histogram(data_array, bins=k) #Make histogram
 	expected_freq = [0]*len(freq)
 	kai =0
 	
@@ -28,9 +23,27 @@ for i in range(M):
 		
 	kai = [((f-e)**2)/e for f, e in zip(freq, expected_freq)]
 	kai = sum(kai)
-	kai_list.append(kai)
+	return kai
 
-bins = range(50)
-plt.hist(kai_list, bins=bins, density=True, label="χ^2分布") #Plot histogram
-plt.plot(bins, chi2.pdf(bins, k-1), label="自由度k-1のχ^2分布") #Plot chi-squrare distribution (degrees of freedom N-1)
-plt.savefig("kadai51.png")
+
+def main():
+	#Get user's input
+	N = int(input("Please input total number of data: "))
+	M = int(input("Please input the repetition time: "))
+	
+	chi_list = []
+
+	for i in range(M):
+		num_list = np.random.randn(N) #Generate N random numbers
+		chi = calc_chi(num_list)
+		chi_list.append(chi)
+
+	bins = range(50)
+	k = int(1 + math.log2(N)) #Sturges' formula
+	plt.hist(chi_list, bins=bins, density=True, label="χ^2分布") #Plot histogram
+	plt.plot(bins, chi2.pdf(bins, k-1), label="自由度k-1のχ^2分布") #Plot chi-squrare distribution (degrees of freedom N-1)
+	plt.savefig("kadai51.png")
+
+
+if __name__ == '__main__':
+	main()
