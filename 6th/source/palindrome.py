7a94ad4a1998bf5dc871118e151374754cfe73b9
Sarah Teoh <you@example.com>
2020-06-05 21:46:56 +0800

    Implement palindrome checker for string

--- 6th/source/palindrome.py
+++ 6th/source/palindrome.py
@@ -6,14 +6,18 @@ def main():
 	
 	palindrome_num = 0
 	palindrome = []
-	with open(path+filename) as f:
-    	for string in f:
-			if(string==string[::-1]):
-				palindrome_num += 1
-				palindrome.append(strinig)
-			else:
-			     continue
+	with open(path+filename, 'r') as f:
+		texts = [line.rstrip('\n') for line in f]
+		
+	for word in texts:
+		# word[::-1] is reversed word
+		if(word==word[::-1]):
+			palindrome_num += 1
+			palindrome.append(word)
+		else:
+		     continue
 
+	print("Total palindrome: ", palindrome_num)
 	print(palindrome)
 
 if __name__ == '__main__':
