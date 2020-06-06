6be87101c86d984555a3e5cab0d94ec6be23c415
Sarah Teoh <you@example.com>
2020-06-06 13:43:51 +0800

    Implement program to find key of caeser cipher

---
+++ 6th/source/caeser_cipher_2.py
@@ -0,0 +1,33 @@
+#課題10-2
+from caeser_cipher import *
+
+if __name__=="__main__":
+	with open("../data/enc_wc.txt", "r") as f:
+		first_few_lines_list = [f.readline() for i in range(3)] # Read first 3 lines
+		first_few_lines = "".join(first_few_lines_list) # Join the first 3 lines
+
+		# Loop through all possible keys between 1 and N 
+		for key in range(1, N):
+			# Try to decrypt the first few lines with ket
+			decrypted = decrypt(first_few_lines , key)
+			print("Key: ", key)
+			# Print out the decrypted first few lines to check whether key used is valid
+			print("Decrypted first few lines:", decrypted, "\n")
+			# Choose to try another possible key if the key was wrong, continue to decrypt whole passage if correct key
+			continue_annot = int(input("Please input 0 to continue to try the next possible key, 1 to decrypt whole passage: "))
+			# If correct key
+			if continue_annot:
+				print("Key Found: ", key)
+				break
+			# If wrong key then continue to try next possible key
+			else:
+				continue
+
+		# Read the whole passage, decrypt and print out
+		leftover_lines = f.read().rstrip("\n")
+		decrypted_leftover = decrypt(leftover_lines, key)
+		decrypted += decrypted_leftover
+		print("Original text: ")
+		print(decrypted)
+
+		
